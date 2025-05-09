cf68726ae8fcd2d29ed466f1e36281c347bb177e
Sebastian <ruben.sebastian.aleman@gmail.com>
2025-05-09 02:00:07 -0400

    bacteria game start

--- bacteria/sketch.js
+++ bacteria/sketch.js
@@ -1,18 +1,45 @@
 class Bacterium {
-    initialize(xPos, yPos) {
+    constructor(xPos, yPos, speed, direction, affiliation) {
         this.xPos = xPos
         this.yPos = yPos
+        this.speed = speed
+        this.direction = direction
+        //affiliation is 'foe' or 'friend'
+        this.affiliation = affiliation
+    }
+
+    updatePosition() {
+        this.xPos = 2
     }
 }
 
+class Morsel {
+    constructor(xPos, yPos, amount) {
+        this.xPos = xPos
+        this.yPos = yPos
+        this.amount = amount
+    }
+}
+
+class UserBacterium {
+    constructor(food) {
+        this.food = food
+    }
+}
+
+let userBacterium = new UserBacterium(30)
+
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+    let width = Math.min(windowWidth, 1200)
+    let height = Math.min(windowHeight, 600)
+    createCanvas(width, height)
     colorMode(HSB, 255)
     background(150)
+    circle(width / 2, height / 2, 150)
+    textAlign(CENTER, CENTER)
+    text('You', width / 2, height / 2)
 }
 
 function draw() {
-    fill(25)
-    noStroke()
-    circle(200, 200, 100)
+    text(userBacterium.food + " food", 50, 50)
 }
